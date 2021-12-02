095a8e0c1960a9e59cb451af33ff36da3dd2c64d
Kay Lai <k@iiot.io>
2021-12-02 13:08:00 +0000

    feat: add the sciprts

---
+++ setup.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+INSTALL_PATH=$( cd $(dirname $0); pwd )
+
+echo "Install path is $INSTALL_PATH"
+echo "Now, seting up your bash environment..."
+echo ""
+
+# set PATH so it includes user's private bin if it exists
+
+if [ -d "$INSTALL_PATH/bin" ] ; then
+  echo 'INSTALL_PATH=$( cd $(dirname $0); pwd )' >> ~/.bashrc
+  echo 'export PATH="$INSTALL_PATH/bin:$PATH"' >> ~/.bashrc
+  source ~/.bashrc
+fi
